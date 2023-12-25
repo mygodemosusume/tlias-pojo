873b8813532b147a17a0e18daedb2f50a732d70d
yuhong <yuhong@qq.com>
2023-12-26 00:36:05 +0800

    第六次提交

--- src/main/java/com/yuhong/pojo/Result.java
+++ src/main/java/com/yuhong/pojo/Result.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Result {
     private Integer code;//响应码，1 代表成功; 0 代表失败
-    private String msg;  //响应信息 描述字符串
-    private Object data; //返回数据hongyu
+    private String msg;  //响应信息 描述字符串hongyu
+    private Object data; //返回数据
 
     //增删改 成功响应
     public static Result success(){
